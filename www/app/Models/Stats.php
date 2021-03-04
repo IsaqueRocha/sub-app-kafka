b4f4ea63e4e08f9da4b1e3c94b028b25cb8e4bc0
Isaque Rocha <isaquerocha@gmail.com>
2021-03-04 04:44:14 -0300

    feat: adiciona o comando consumidor de mensagens

---
+++ www/app/Models/Stats.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Stats extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'stats';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'inventory_id',
+        'make',
+        'model'
+    ];
+}
